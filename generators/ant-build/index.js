58d347175cda00380c6ebbb5e24f1a87160315b6
Jibé Barth <barth.jib@gmail.com>
2016-03-17 17:49:28 +0100

    Can call dynamically named function for ant choice

--- generators/ant-build/index.js
+++ generators/ant-build/index.js
@@ -3,6 +3,7 @@ var yeoman = require('yeoman-generator');
 var chalk = require('chalk');
 var yosay = require('yosay');
 
+
 module.exports = yeoman.generators.Base.extend({
   prompting: function () {
     var done = this.async();
@@ -12,26 +13,48 @@ module.exports = yeoman.generators.Base.extend({
       'Welcome to the impeccable ' + chalk.red('generator-happy') + ' generator!'
     ));
 
+    var subTypeAnt = [
+      {name:"Ant build for prestashop", value:'prestashop'},
+      {name:"Ant build for prestamodule", value:'prestamodule'},
+      {name:"Ant build for Symfony", value:'symfony'},
+      {name:"Ant build for Haxe project", value:'haxe'},
+    ];
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'list',
+      name: 'antChoice',
+      message:'What kind of build.xml you want',
+      default: 0,
+      choices: subTypeAnt,
     }];
 
     this.prompt(prompts, function (props) {
-      this.props = props;
+      this["_ask_"+props.antChoice]();
+      //this.props = props;
       // To access props later use this.props.someOption;
 
       done();
     }.bind(this));
   },
 
+  _ask_prestashop: function(){
+    this.log("Let's go for prestashop ant build");
+  },
+  _ask_prestamodule: function(){
+    this.log("Let's go for prestashop module ant build");
+
+  },
+  _ask_symfony: function(){
+    this.log("Let's go for symfony ant build");
+
+  },
+  _ask_haxe:function(){
+    this.log("Let's go for haxe ant build");
+  },
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    // this.fs.copy(
+    //   this.templatePath('dummyfile.txt'),
+    //   this.destinationPath('dummyfile.txt')
+    // );
   },
 
   install: function () {
